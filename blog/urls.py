1c7196f42d50352a7731e0683a538062352c4094
kyungjunlee <kyungjunlee.me@gmail.com>
2021-08-14 14:11:48 +0900

    auth_preparation

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from blog import views
+
+
+app_name = "blog"  # URL Reverse에서 namespace역할을 하게 됨
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
+]
