ef57f638c710f282988223c03574e970b28ec0e2
kyungjunlee <kyungjunlee.me@gmail.com>
2021-08-15 11:10:08 +0900

    function_04_profile_signals

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+from django.conf import settings
+from django.db.models.signals import post_save
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    website_url = models.URLField(blank=True)
---
+++ accounts/signals.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from accounts.models import Profile
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def on_post_save_for_user(sender, **kwargs):
+    if kwargs["created"]:
+        user = kwargs["instance"]
+        Profile.objects.create(user=user)
