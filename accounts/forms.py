44f23e12d22dea6d30d6b50ab43e9c0b20b213bd
kyungjunlee <kyungjunlee.me@gmail.com>
2021-08-15 16:43:17 +0900

    function_06_e-mail 형태로 로그인하기

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,51 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.core.validators import validate_email
 
 
 class SignupForm(UserCreationForm):
-    pass
+    # 각 필드에 대한 validators, clean_필드명, clean(멤버함수)
+    # https://github.com/django/django/blob/main/django/contrib/auth/models.py#L321
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.fields["username"] 는 아래와 같음
+        # <django.contrib.auth.forms.UsernameField object at 0x11009b220>
+        # <django.contrib.auth.forms.UsernameField object at 0x11009b220>
+        # self.fields["username"].__dict__
+        # special variables:
+        # function variables:
+        # 'max_length': 150
+        # 'min_length': None
+        # 'strip': True
+        # 'empty_value': ''
+        # 'required': True
+        # 'label': 'Username'
+        # 'initial': None
+        # 'show_hidden_initial': False
+        # 'help_text': 'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'
+        # 'disabled': False
+        # 'label_suffix': None
+        # 'localize': False
+        # 'widget': <django.forms.widgets.TextInput object at 0x11009bc40>
+        # 'error_messages': {'required': 'This field is required.'}
+        # 'validators': [<django.core.validat...10f460220>]
+        # len(): 15
+
+        self.fields["username"].validators = [validate_email]
+        self.fields["username"].help_text = "Enter Email Format"
+        self.fields["username"].label = "Email"
+        # self.fields["username"].widget = "Email"
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = user.username
+        if commit:
+            user.save()
+        return user
+
+    # # clean_필드명으로 유효성 검사한 방식
+    # def clean_username(self):
+    #     value = self.cleaned_data.get("username")
+    #     if value:
+    #         validate_email(value)
+    #     return value
--- djangocontribauth/settings.py
+++ djangocontribauth/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en-us"  # 현재 시스템에 대한 디폴트 언어설정 (사용자마다 다르게 적용 가능)
 
 TIME_ZONE = "UTC"
 
@@ -123,11 +123,9 @@ STATIC_URL = "/static/"
 
 from django.urls import reverse_lazy
 
-LOGIN_URL = reverse_lazy('login')
+LOGIN_URL = reverse_lazy("login")
 # LOGIN_REDIRECT_URL = reverse_lazy('profile')
-LOGOUT_REDIRECT_URL = reverse_lazy('login')
-
-
+LOGOUT_REDIRECT_URL = reverse_lazy("login")
 
 
 # Default primary key field type
