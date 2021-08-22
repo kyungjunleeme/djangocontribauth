5012bf97b535cca3ecd1ef5d457aa0e761355dfb
kyungjunlee <kyungjunlee.me@gmail.com>
2021-08-22 20:50:59 +0900

    selet_related, prefetch_related apply to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 from .models import User, Profile
 from .forms import CustomUserForm
 
@@ -10,6 +12,17 @@ class UserAdmin(admin.ModelAdmin):
     form = CustomUserForm
 
 
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     list_display = ["user", "bio"]
+
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ["user", "bio"]
+    list_display = [profile.name for profile in Profile._meta.fields]
+    # list_select_related = ['user']
+
+    # def get_queryset(self, request: HttpRequest) -> QuerySet:
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        qs = super().get_queryset(request)
+        return qs.select_related("user")
